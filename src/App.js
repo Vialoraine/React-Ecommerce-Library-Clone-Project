fdab4f3ea3bb22bdb6b18eb1a7939d8250ff12e6
Loreina Salalila <vialoraine.gg@gmail.com>
2024-05-20 20:59:01 -0700

    Home page created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import { Discounted } from "./components/Discounted";
-import { Explore } from "./components/Explore";
-import { Featured } from "./components/Featured";
 import { Footer } from "./components/Footer";
-import { Highlights } from "./components/Highlights";
-import { Landing } from "./components/Landing";
 import { Nav } from "./components/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Home />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
