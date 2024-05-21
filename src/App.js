3488ecdd3cf4c728c8c07653f5be3b09ba86efa1
Loreina Salalila <vialoraine.gg@gmail.com>
2024-05-20 21:29:25 -0700

    Books page created

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { Footer } from "./components/Footer";
 import { Nav } from "./components/Nav";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Books } from "./pages/Books";
+import { books } from "./data";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Nav />
-        <Home />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" render={() => <Books books={books} />} />
         <Footer />
       </div>
     </Router>
