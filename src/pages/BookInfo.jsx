d22bdd558d0edf05e33f12fcf5dc72fca14a6978
Loreina Salalila <vialoraine.gg@gmail.com>
2024-05-20 23:38:43 -0700

    debugging runtime error on BookInfo.js

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,12 +1,11 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Link, useParams } from "react-router-dom";
-import { Price } from "../components/ui/Price";
 import { Rating } from "../components/ui/Rating";
+import { Price } from "../components/ui/Price";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useParams, Link } from "react-router-dom";
 
 export const BookInfo = ({ books }) => {
   const { id } = useParams();
-  const book = books.find((book) => +book.id === +id);
 
   return (
     <div id="books__body">
@@ -23,49 +22,54 @@ export const BookInfo = ({ books }) => {
             </div>
             <div className="book__selected">
               <figure className="book__selected--figure">
-                <img className="book__selected--img" src={book.url} alt="" />
+                <img
+                  src="https://covers.openlibrary.org/b/id/10958382-L.jpg"
+                  alt=""
+                  className="book__selected--img"
+                />
               </figure>
               <div className="book__selected--description">
-                <h2 className="book__selected--title">{book.title}</h2>
-                <Rating rating={book.rating} />
-                <div className="book__selected--price">
-                  <Price
-                    originalPrice={book.originalPrice}
-                    salePrice={book.salePrice}
-                  />
+                <h2 className="book__selected--title">Atomic Habits</h2>
+                <div className="book__ratings">
+                  <FontAwesomeIcon icon="star" />
+                  <FontAwesomeIcon icon="star" />
+                  <FontAwesomeIcon icon="star" />
+                  <FontAwesomeIcon icon="star" />
+                  <FontAwesomeIcon icon="star" />
                 </div>
+                <div className="book__selected--price">$39.00</div>
                 <div className="book__summary">
                   <h3 className="book__summary--title">Summary</h3>
                   <p className="book__summary--para">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Veniam, repellendus modi odio porro, consequuntur,
-                    asperiores minima sint voluptatem at reiciendis ducimus
-                    neque provident alias iure nihil explicabo nobis id
-                    voluptas.
+                    Perferendis nisi culpa totam. Laudantium iusto praesentium
+                    tempora consequatur repellat aperiam, minima nulla, omnis ex
+                    ipsa architecto magnam, qui eligendi sit deleniti?
                   </p>
                   <p className="book__summary--para">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Veniam, repellendus modi odio porro, consequuntur,
-                    asperiores minima sint voluptatem at reiciendis ducimus
-                    neque provident alias iure nihil explicabo nobis id
-                    voluptas.
+                    Perferendis nisi culpa totam. Laudantium iusto praesentium
+                    tempora consequatur repellat aperiam, minima nulla, omnis ex
+                    ipsa architecto magnam, qui eligendi sit deleniti?
                   </p>
+                  <button className="btn">Add to cart</button>
                 </div>
-                <button className="btn">Add to cart</button>
               </div>
             </div>
           </div>
-        </div>
-        <div className="books__container">
-          <div className="row">
-            <div className="book__selected--top">
-              <h2 className="book__selected--title--top">Recommended Books</h2>
-            </div>
-            <div className="books">
-              <div className="book"></div>
-              <div className="book"></div>
-              <div className="book"></div>
-              <div className="book"></div>
+          <div className="books__container">
+            <div className="row">
+              <div className="book__selected--top">
+                <h2 className="book__selected--title--top">
+                  Recommended Books
+                </h2>
+              </div>
+              <div className="books">
+                <div className="book"></div>
+                <div className="book"></div>
+                <div className="book"></div>
+                <div className="book"></div>
+              </div>
             </div>
           </div>
         </div>
