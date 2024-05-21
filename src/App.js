e6c009ca8d4aaf7225e473e3501c3d97061cbd18
Loreina Salalila <vialoraine.gg@gmail.com>
2024-05-20 18:39:46 -0700

    Landing component completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { Landing } from "./components/Landing";
 import { Nav } from "./components/Nav";
+
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      Test
+      <Landing />
     </div>
   );
 }
