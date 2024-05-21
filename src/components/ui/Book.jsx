1a8cda382d147f1af7b08775b764da39f9719aeb
Loreina Salalila <vialoraine.gg@gmail.com>
2024-05-21 03:20:36 -0700

    Book functionality completed

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,23 +1,56 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { Rating } from "./Rating";
 import { Price } from "./Price";
 
 export const Book = ({ book }) => {
+  const [img, setImg] = useState();
+
+  const mountedRef = useRef(true);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = book.url;
+    image.onload = () => {
+      setTimeout(() => {
+        if (mountedRef.current) {
+          setImg(image);
+        }
+      }, 300);
+    };
+    return () => {
+      mountedRef.current = false;
+    };
+  }, [book.url]);
+
   return (
     <div className="book">
-      <Link to={`/books/${book.id}`}>
-        <figure className="book__img--wrapper">
-          <img className="book__img" src={book.url} alt="" />
-        </figure>
-      </Link>
-      <div className="book__title">
-        <Link to={`/books/${book.id}`} className="book__title--link">
-          {book.title}
-        </Link>
-      </div>
-      <Rating rating={book.rating} />
-      <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+      {img ? (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book__img--wrapper">
+              <img className="book__img" src={img.src} alt="" />
+            </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+          <Rating rating={book.rating} />
+          <Price
+            originalPrice={book.originalPrice}
+            salePrice={book.salePrice}
+          />
+        </>
+      ) : (
+        <>
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div className="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>
+        </>
+      )}
     </div>
   );
 };
